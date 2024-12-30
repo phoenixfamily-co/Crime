884a1597b898879b09c9a1e2b2418daabe26736a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-12-30 21:40:58 +0330

    Home added

--- CrimeProject/settings.py
+++ CrimeProject/settings.py
@@ -86,7 +86,6 @@ DATABASES = {
         'PORT': '3306',                # پورت MySQL (پورت پیش‌فرض: 3306)
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
         },
     }
 }
