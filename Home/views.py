c2912b780a36a1c031d225792eb61d2300432cf0
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-12-30 06:25:06 +0330

    Home added

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.utils.translation import get_language
+from django.utils.translation import get_language, get_language_bidi
 from django.views.decorators.cache import cache_page
+
+from Product.models import Case
 
 
 @cache_page(60 * 15)
 def home(request):
     current_language = get_language()
-    return render(request, 'home.html', {'LANGUAGE_CODE': current_language})
+    is_bidi = get_language_bidi()
+
+    products = Case.objects.all()
+
+    return render(request, 'home.html', {'LANGUAGE_CODE': current_language,'LANGUAGE_BIDI': is_bidi,'products':products})
