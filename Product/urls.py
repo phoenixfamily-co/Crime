365e4790b001ebfab22d2ec945d534964bb9007b
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2024-12-30 06:32:01 +0330

    Home added

--- Product/urls.py
+++ Product/urls.py
@@ -13,7 +13,7 @@ router.register(r'evidences', EvidenceViewSet)
 router.register(r'interrogations', InterrogationViewSet)
 
 urlpatterns = [
-    path('start/', views.start, name='start-view'),  # صفحه اصلی
+    path('start/<int:pk>/', views.start, name='start-view'),  # صفحه اصلی
     path('suspects/', views.suspects, name='suspects-view'),  # صفحه اصلی
     path('interrogation/', views.interrogation, name='interrogation-view'),  # صفحه اصلی
     path('evidence/', views.evidence, name='evidence-view'),  # صفحه اصلی
