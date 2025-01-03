d2995a6d98dd72623317e0f7665e530f5b06b7a5
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-04 01:22:54 +0330

    gender display changed

--- Product/models.py
+++ Product/models.py
@@ -39,8 +39,8 @@ class Case(models.Model):
         verbose_name="سطح سختی"
     )
     score = models.IntegerField(default=0, verbose_name="امتیاز", blank=True, null=True)
-    category = models.ForeignKey(CaseCategory, on_delete=models.CASCADE, verbose_name="دسته‌بندی پرونده", null=True,
-                                 blank=True)
+    # category = models.ForeignKey(CaseCategory, on_delete=models.CASCADE, verbose_name="دسته‌بندی پرونده", null=True,
+    #                              blank=True)
 
     def __str__(self):
         return self.title
