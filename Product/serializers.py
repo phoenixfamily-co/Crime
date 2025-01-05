9b17affdc3953a291358e1973b1323d1990fe197
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-05 08:32:16 +0330

    gender display changed

--- Product/serializers.py
+++ Product/serializers.py
@@ -12,7 +12,7 @@ class CaseSerializer(serializers.ModelSerializer):
 class SuspectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Suspect
-        fields = ['id', 'name', 'age', 'height', 'weight', 'blood_type', 'marital_status', 'case',
+        fields = ['id', 'name', 'age', 'height', 'weight', 'blood_type', 'marital_status', 'case', 'role',
                   'job', 'gender', 'relationship_with_victim', 'excuse', 'last_seen', 'image', 'video']
 
 
