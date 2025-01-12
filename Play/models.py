636e389d2d919dd360d8a18776159e5eed62b97e
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-12 18:18:14 +0330

    gender display changed

--- Play/models.py
+++ Play/models.py
@@ -7,7 +7,7 @@ from User.models import User
 
 class CasePlay(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    orderItem = models.ForeignKey(OrderItem, on_delete=models.CASCADE, blank=True, null=True)
+    # orderItem = models.ForeignKey(OrderItem, on_delete=models.CASCADE, blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=50, choices=[('in_progress', 'In Progress'), ('completed', 'Completed'),
@@ -28,7 +28,7 @@ class CaeParticipant(models.Model):
 
 
 class GameResult(models.Model):
-    gameplay = models.ForeignKey(CasePlay, on_delete=models.CASCADE)
+    gameplay = models.ForeignKey(CasePlay, on_delete=models.CASCADE, blank=True, null=True)
     suspect = models.ForeignKey(Suspect, on_delete=models.CASCADE, blank=True, null=True)
     reason = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=50, choices=[('success', 'Success'), ('failed', 'Failed')])
