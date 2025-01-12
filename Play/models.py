e1ac43b0589e52a64418f579fa65d9740fd75054
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-12 18:24:05 +0330

    gender display changed

--- Play/models.py
+++ Play/models.py
@@ -7,15 +7,13 @@ from User.models import User
 
 class CasePlay(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    orderItem = models.ForeignKey(OrderItem, on_delete=models.CASCADE, blank=True, null=True)
+    # orderItem = models.ForeignKey(OrderItem, on_delete=models.CASCADE, blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=50, choices=[('in_progress', 'In Progress'), ('completed', 'Completed'),
                                                       ('cancelled', 'Cancelled')],
                               default='in_progress')
 
-    def __str__(self):
-        return f"GamePlay of {self.orderItem.case.title} by {self.user.number}"
 
 
 class CaeParticipant(models.Model):
@@ -28,7 +26,7 @@ class CaeParticipant(models.Model):
 
 
 class GameResult(models.Model):
-    # gameplay = models.ForeignKey(CasePlay, on_delete=models.CASCADE, blank=True, null=True)
+    gameplay = models.ForeignKey(CasePlay, on_delete=models.CASCADE, blank=True, null=True)
     suspect = models.ForeignKey(Suspect, on_delete=models.CASCADE, blank=True, null=True)
     reason = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=50, choices=[('success', 'Success'), ('failed', 'Failed')])
