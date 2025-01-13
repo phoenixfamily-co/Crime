3a3368439ffaf5855243d2c689c342a400265992
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-13 09:56:54 +0330

    gender display changed

--- Product/views.py
+++ Product/views.py
@@ -4,8 +4,6 @@ from django.utils.translation import get_language, get_language_bidi
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-
-from Play.views import start_case_play
 from Product.models import Case, Suspect, Interrogation, Evidence
 from Product.serializers import CaseSerializer, SuspectSerializer, InterrogationSerializer, EvidenceSerializer
 from User.views import get_or_create_temporary_user, save_user_device_info, log_user_activity
@@ -30,7 +28,6 @@ def start(request, pk):
     user = get_or_create_temporary_user(request)
     save_user_device_info(request, user)
     log = log_user_activity(request, request.build_absolute_uri(), user)
-    start_case_play(request,user)
     is_bidi = get_language_bidi()
     product = get_object_or_404(Case, id=pk)
 
