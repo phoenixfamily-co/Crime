9b3a507f2ac1b8bec90b7a42857ea901c4e0e40a
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-01-14 05:27:43 +0330

    finished

--- Play/urls.py
+++ Play/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GameResultViewSet, CasePlayViewSet
+from .views import GameResultViewSet, CasePlayViewSet, success , failed
 
 app_name = 'play'
 
@@ -10,5 +10,8 @@ router.register(r'result', GameResultViewSet)
 router.register(r'start', CasePlayViewSet)
 
 urlpatterns = [
+    path('success/<int:pk>/', success, name='success-view'),  # صفحه اصلی
+    path('failed/<int:pk>/', failed, name='failed-view'),  # صفحه اصلی
+
     path('api/', include(router.urls)),
 ]
