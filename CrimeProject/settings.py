d038c2c1cfe9272c7a6415f1d6ab668a1e7cff18
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-03-07 03:48:03 +0330

    finished

--- CrimeProject/settings.py
+++ CrimeProject/settings.py
@@ -89,6 +89,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',  # فعال‌سازی پردازشگر i18n
+                'django.contrib.auth.context_processors.auth',
             ],
         },
     },
