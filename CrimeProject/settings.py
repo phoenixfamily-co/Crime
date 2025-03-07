f59a11f4a002d4767213dbe2a29c23e40ac182ec
abbas lotfinasab <abbas.lotfinasab2024@gmail.com>
2025-03-07 03:36:45 +0330

    finished

--- CrimeProject/settings.py
+++ CrimeProject/settings.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,6 +14,22 @@ SECRET_KEY = 'django-insecure-=56+47c2k8yph88k#z_89w@j7-3o-1*1^48ig=g^w$u+c=rizm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+DOMAIN = 'crime.phoenixfamily.co'
+SITE_NAME = 'crime'
+SITE_URL = "https://crime.phoenixfamily.co"
+
+SITE_ID = 1
+
+META_SITE_PROTOCOL = "https"
+META_USE_OG_PROPERTIES = True
+META_USE_TWITTER_PROPERTIES = True
+META_USE_SCHEMAORG_PROPERTIES = True
+
+CSRF_TRUSTED_ORIGINS = ["https://crime.phoenixfamily.co", "https://www.crime.phoenixfamily.co"]
+
 ALLOWED_HOSTS = [
     'localhost',
     'crime.phoenixfamily.co',
@@ -22,12 +37,16 @@ ALLOWED_HOSTS = [
 ]
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',  # برای ساخت Sitemap
+    'django.contrib.humanize',  # برای نمایش بهتر اعداد و تاریخ‌ها
     'django_user_agents',
     'rest_framework',
     'django_extensions',
@@ -84,16 +103,44 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'phoenixf_crime',  # نام دیتابیس
-        'USER': 'phoenixf_crime',       # نام کاربری دیتابیس
-        'PASSWORD': 's=4deM.C}9O3',   # رمز عبور دیتابیس
-        'HOST': 'localhost',           # آدرس سرور MySQL (یا آدرس دلخواه)
-        'PORT': '3306',                # پورت MySQL (پورت پیش‌فرض: 3306)
+        'USER': 'phoenixf_crime',  # نام کاربری دیتابیس
+        'PASSWORD': 's=4deM.C}9O3',  # رمز عبور دیتابیس
+        'HOST': 'localhost',  # آدرس سرور MySQL (یا آدرس دلخواه)
+        'PORT': '3306',  # پورت MySQL (پورت پیش‌فرض: 3306)
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
 
+SEO = {
+    'default': {
+        'title': _('Phoenix Family | Board Games, Entertainment Apps & More!'),
+        'description': _(
+            'Discover Phoenix Family – your ultimate destination for family entertainment! From online entertainment to'
+            ' entertainment apps and best board games for adults, fun awaits!'),
+        'keywords': [
+            _('phoenix family'),
+            _('family entertainment'),
+            _('entertainment online'),
+            _('entertainment apps'),
+            _('family board games'),
+            _('board game online'),
+            _('board game apps'),
+            _('game apps'),
+            _('best entertainment apps'),
+            _('best board games for adults'),
+        ],
+        'robots': 'index, follow',  # یا 'noindex, nofollow' برای جلوگیری از ایندکس
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -117,16 +164,33 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGES = [
-    ('fa', _('Farsi')),
+    ('en', _('English')),
+    ('fa', _('Persian')),
+    ('ar', _('Arabic')),
+    ('tr', _('Turkish')),
+    ('ru', _('Russian')),
+    ('hi', _('Hindi')),
+    ('ja', _('Japanese')),
+    ('ko', _('Korean')),
+    # ('zh-hans', _('Simplified Chinese')),  # چینی ماندارین ساده‌شده
+
 ]
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',  # مسیر پوشه‌ی locale
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -141,6 +205,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = '/home/phoenixf/crime.phoenixfamily.co/media/'
 MEDIA_URL = '/media/'
 
+ROBOTS_FILE_PATH = '/home/abbaslot/public_html/robots.txt/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -152,12 +218,28 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',
+            'encoding': 'utf-8',
         },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',  # سطح لاگ‌ها (DEBUG، INFO، WARNING، ERROR، CRITICAL)
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.phoenixfamily.co'
+EMAIL_PORT = 465  # پورت SMTP
+EMAIL_USE_TLS = False  # TLS غیرفعال است
+EMAIL_USE_SSL = True  # SSL فعال است
+EMAIL_HOST_USER = 'customer@phoenixfamily.co'
+EMAIL_HOST_PASSWORD = 'n.Y?8&eg0$60'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+COMPANY_EMAIL = 'info@phoenixfamily.co'
